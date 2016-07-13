91c23c04bab29a3b4c6b6ce831e7e92a9649ecd9
pxn <nghi.tran@lazada.com>
2016-07-13 20:59:35 +0700

    finish MS 5 before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_action :require_login
 
   helper_method :current_user, :signed_in?
 
@@ -12,5 +12,18 @@ class ApplicationController < ActionController::Base
 
   def signed_in?
     !!current_user
+  end
+
+  def require_login
+    # if signed_in?
+    #   redirect_to user_path
+    # else
+    #   flash[:error] = "You must sign in to see this page!"
+    #   redirect_to login_path
+    # end
+    unless signed_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to login_url # halts request cycle
+    end
   end
 end
