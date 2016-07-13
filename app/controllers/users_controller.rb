5b6c85698ef21a6eeb66ac26c889c7f629b46a64
pxn <nghi.tran@lazada.com>
2016-07-13 19:52:38 +0700

    register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
   def index
     @users = User.all.shuffle
   end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = 'User created successfully.'
+      redirect_to root_path
+    else
+      flash.now[:error] = "Error: #{@user.errors.full_messages.to_sentence}"
+      redirect_to 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
