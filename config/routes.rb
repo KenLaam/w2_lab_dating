c243c0d66fa505da0d0960f975816febf6103c8f
pxn <nghi.tran@lazada.com>
2016-07-13 20:36:08 +0700

    finish MS 3

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # get 'sessions/new'
   resources :sessions, only: [:new, :create]
   get 'login' => 'sessions#new'
+  delete 'logout' => 'sessions#destroy'
   root 'welcome#index'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
