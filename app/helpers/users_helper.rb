1e923aaca674f955158a8e20098265cecd582b2f
pxn <nghi.tran@lazada.com>
2016-07-13 20:02:53 +0700

    default_image_url

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  def default_image_url(user)
+    return unless user
+    user.image_url.presence || 'http://4.bp.blogspot.com/_ndu1QvzN_g4/TGyv2ll3qJI/AAAAAAAAAJs/ZWV9JPibojA/s72-c/fb-default-pics-2.jpg'
+  end
 end
